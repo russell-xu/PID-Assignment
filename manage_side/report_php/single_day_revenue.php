0f3d573422bef77904bc0302324ed6e04d08a591
throuz <64568129+throuz@users.noreply.github.com>
2020-09-07 23:40:50 +0800

    Complete basic report

--- manage_side/report_php/single_day_revenue.php
+++ manage_side/report_php/single_day_revenue.php
@@ -1,14 +1,14 @@
 <?php
-require_once("../connectconfig.php");
+require_once("../../connectconfig.php");
 
 $sql_single_day_revenue = <<<multi
 SELECT
-    DATE_FORMAT(`date`, '%y-%m-%d') AS `datetime`,
-    SUM(`total_price`)
+    DATE_FORMAT(`date`, '%Y-%m-%d') AS `datetime`,
+    SUM(`total_price`) AS `total_price`
 FROM
     orders
 GROUP BY
     `datetime`
 multi;
-$rows = $link->query($sql_single_day_revenue)->fetch_row();
+$rows = $link->query($sql_single_day_revenue)->fetch_all(MYSQLI_ASSOC);
 echo json_encode($rows, true);
